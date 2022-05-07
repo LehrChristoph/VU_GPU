b4c269bfc52abada5aaa3ea13e25e58ee77d3ef0
Lukas Rysavy <lukas@rysavy.net>
2022-05-07 20:32:44 +0200

    project: working gpu implementation

--- project/src/impl/gpu_thread_per_cc.cu
+++ project/src/impl/gpu_thread_per_cc.cu
@@ -9,19 +9,19 @@ extern "C" {
 }
 #include "cuda_helpers.h"
 
-__global__ void populateGraph(dense_graph *d_graph, void *current_base,
-                              void *real_base) {
+__global__ void populateGraph(dense_graph *d_graph, dense_node *d_nodes,
+                              dense_edge *d_edges, void *current_base) {
   int node = blockIdx.x * blockDim.x * blockDim.y + threadIdx.x * blockDim.y +
              threadIdx.y;
-  if (node > d_graph->num_nodes)
+  if (node >= d_graph->num_nodes)
     return;
   if (node == 0) {
-      d_graph->nodes = (dense_node *)(d_graph + 1);
+    d_graph->nodes = d_nodes;
   }
+
+  ptrdiff_t idx = d_nodes[node].edges - d_nodes[0].edges;
   __syncthreads();
-  d_graph->nodes[node].edges =
-      (dense_edge *)((uint64_t)d_graph->nodes[node].edges +
-                     (uint64_t)real_base - (uint64_t)current_base);
+  d_graph->nodes[node].edges = &d_edges[idx];
   __syncthreads();
 }
 
@@ -29,8 +29,8 @@ __global__ void calculate(dense_graph *d_graph,
                           connected_components *d_components,
                           component *d_comps, int *d_nodes) {
   int node = blockIdx.x * blockDim.x * blockDim.y + threadIdx.x * blockDim.y +
-             threadIdx.y - 1;
-  if (node > d_graph->num_nodes)
+             threadIdx.y;
+  if (node >= d_graph->num_nodes)
     return;
   d_comps[node].num_nodes = 0;
   int *worklist = d_nodes + node * d_graph->num_nodes;
@@ -41,11 +41,12 @@ __global__ void calculate(dense_graph *d_graph,
     wl_len--;
     for (int i = 0; i < d_graph->nodes[curr].num_edges; i++) {
       int target = d_graph->nodes[curr].edges[i];
-      if (target > node) { // not handled by this thread
+      if (target < node) { // not handled by this thread
+        d_comps[node].num_nodes = 0;
         return;
       }
       bool seen = false;
-      for (int j = 0; j < wl_pos; j++) {
+      for (int j = 0; j < wl_pos + wl_len; j++) {
         if (worklist[j] == target) {
           seen = true;
           break;
@@ -56,8 +57,8 @@ __global__ void calculate(dense_graph *d_graph,
       worklist[wl_pos + wl_len++] = target;
     }
   }
-  d_comps[node].num_nodes = wl_pos - 1;
-  d_comps[node].nodes = d_nodes + node * d_graph->num_nodes + 1;
+  d_comps[node].num_nodes = wl_pos;
+  d_comps[node].nodes = d_nodes + node * d_graph->num_nodes;
   atomicInc(&d_components->num_components, d_graph->num_nodes);
 }
 
@@ -65,21 +66,20 @@ extern "C" {
 clock_t connected_components_thread_per_cc(dense_graph *graph,
                                            connected_components **out) {
   dense_graph *d_graph;
-  CHECK(cudaMalloc((void **)&d_graph,
-                   sizeof(dense_graph) + sizeof(dense_node) * graph->num_nodes +
-                       sizeof(dense_edge) * graph->num_edges));
-  CHECK(cudaMemcpy(d_graph, graph,
-                   sizeof(dense_graph) + sizeof(dense_node) * graph->num_nodes +
-                       sizeof(dense_edge) * graph->num_edges,
-                   cudaMemcpyHostToDevice));
+  dense_node *d_gnodes;
+  dense_edge *d_edges;
+  CHECK(cudaMalloc((void **)&d_graph, sizeof(dense_graph)));
+  CHECK(cudaMalloc((void **)&d_gnodes, sizeof(dense_node) * graph->num_nodes));
+  CHECK(cudaMalloc((void **)&d_edges, sizeof(dense_edge) * graph->num_edges * 2));
+  CHECK(cudaMemcpy(d_graph, graph, sizeof(dense_graph), cudaMemcpyHostToDevice));
+  CHECK(cudaMemcpy(d_gnodes, graph->nodes, sizeof(dense_node) * graph->num_nodes, cudaMemcpyHostToDevice));
+  CHECK(cudaMemcpy(d_edges, graph->nodes->edges, sizeof(dense_edge) * graph->num_edges * 2, cudaMemcpyHostToDevice));
   dim3 block, grid;
   block.x = 32;
   block.y = 32;
   grid.x = ceil((double)graph->num_nodes / block.x / block.y);
   grid.y = 1;
-  populateGraph<<<grid, block>>>(d_graph, graph->nodes->edges,
-                                 d_graph + sizeof(dense_graph) +
-                                     sizeof(dense_node) * graph->num_nodes);
+  populateGraph<<<grid, block>>>(d_graph, d_gnodes, d_edges, graph->nodes->edges);
   cudaDeviceSynchronize();
   connected_components *d_components;
   CHECK(cudaMalloc((void **)&d_components, sizeof(connected_components)));
@@ -98,7 +98,7 @@ clock_t connected_components_thread_per_cc(dense_graph *graph,
                    cudaMemcpyDeviceToHost));
 
   components->components =
-      (component *)malloc(sizeof(component) * components->num_components);
+      (component *)malloc(sizeof(component) * graph->num_nodes);
   CHECK(cudaMemcpy(components->components, d_comps,
                    sizeof(component) * graph->num_nodes, cudaMemcpyDeviceToHost));
 
@@ -112,12 +112,15 @@ clock_t connected_components_thread_per_cc(dense_graph *graph,
     CHECK(cudaMemcpy(components->components[ind].nodes, d_nodes,
                      sizeof(int) * components->components[i].num_nodes,
                      cudaMemcpyDeviceToHost));
+    components->components[ind].num_nodes = components->components[i].num_nodes;
     ind++;
   }
   // free gpu memory
   CHECK(cudaFree(d_nodes));
   CHECK(cudaFree(d_comps));
   CHECK(cudaFree(d_components));
+  CHECK(cudaFree(d_edges));
+  CHECK(cudaFree(d_gnodes));
   CHECK(cudaFree(d_graph));
   *out = components;
   return end - start;
